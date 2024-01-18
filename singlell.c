862d414f0755bd222940465057c3f884a79c66fa
Tamal Bhattacharjee <119165001+TamalBht@users.noreply.github.com>
2024-01-18 17:45:40 +0530

    update

--- singlell.c
+++ singlell.c
@@ -7,4 +7,19 @@ typedef struct Node{
     Node* next;
 }Node;
 Node*list;
+Node* createNode(int data){
+    Node*newNode=(Node*)malloc(sizeof(Node));
+    newNode->data=data;
+    newNode->next=newNode->prev=NULL;
+}
+void insert(Node**head,int data){
+    Node* newNode=createNode(data);
+    Node* current=*head;
+    while(current->next!=NULL){
+        current=current->next;
+    }
+    current->next=newNode;
+    newNode->prev=current; 
+
+}
 
