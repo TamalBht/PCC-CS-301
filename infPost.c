53bf0745a752f868987f531327d44bef95959bee
Tamal Bhattacharjee <119165001+TamalBht@users.noreply.github.com>
2024-01-21 18:07:26 +0530

    change

--- infPost.c
+++ infPost.c
@@ -19,3 +19,45 @@ void push(Stack* s,char c){
 char pop(Stack*s){
     return s->arr[s->top--];
 }
+int isOperator(char c){
+    return(c=='+'||c=='-'||c=='*'||c=='/'||c=='^');
+}
+int precedence(char c){
+    if(c=='+'||c=='-'){
+        return 1;
+    }
+    else if(c=='*'||c=='/'){
+        return 2;
+    }
+    else if(c=='^'){
+        return 3;
+    }
+    else{
+        printf("Invalid operator");
+        exit(0);
+    }
+}
+//in this we are dealing with two strings 
+//the first one is the expression that the user will provide and the second one is the postfix expression in which it will be pushed
+void inf2pf(char *exp,char *post){
+    Stack stack;
+    initial(&stack);
+    char popped;
+    int i,j=0;
+    for(i=0;exp[i]!='\0';i++){
+        char c=exp[i];
+        if(isalnum(c)){
+            post[j++]=c;
+        }
+        else if(c=='('){
+            push(&stack,c);
+        }
+        else if(isOperator(c)){
+            while(isOperator(stack.arr[stack.top]) && precedence(c)<=precedence(stack.arr[stack.top]) && stack.top!=-1){
+				post[j++]=pop(&stack);
+			}
+			push(&stack,c);
+        }
+        
+    }
+}
