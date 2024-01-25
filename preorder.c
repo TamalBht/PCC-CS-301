e69e982de7017fcf4a497631f299c6cc86452a57
Tamal Bhattacharjee <119165001+TamalBht@users.noreply.github.com>
2024-01-25 23:51:26 +0530

    change

--- preorder.c
+++ preorder.c
@@ -1,1 +1,22 @@
 #include<stdio.h>
+#include<stdlib.h>
+typedef struct Node{
+    int data;
+    struct Node* left;
+    struct Node* right;
+}Node;
+Node* initial(Node* n,int inp){
+    Node* newNode = (Node*)malloc(sizeof(Node));
+    newNode->data = inp;
+    newNode->left = NULL;
+    newNode->right = NULL;
+    return newNode;
+}
+void preorder(Node* root){
+    if(root==NULL){
+        return;
+    }
+    print("%d",&root->data);
+    preorder(root->left);
+    preorder(root->right);
+}
