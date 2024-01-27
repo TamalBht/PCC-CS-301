89b747190612446f2f8acde6a84dadc1499c9d39
Tamal Bhattacharjee <119165001+TamalBht@users.noreply.github.com>
2024-01-27 22:11:08 +0530

    change

--- levelOrderTraversal.c
+++ levelOrderTraversal.c
@@ -1,3 +1,15 @@
 #include<stdio.h>
 #include<stdlib.h>
 #define MAX 100
+//Node to represent the tree node
+typedef struct Node{
+    int data;
+    struct Node* left;
+    struct Node* right;
+}Node;
+Node* createNode(int inp){
+    Node* newNode=(Node*)malloc(sizeof(Node));
+    newNode->data=inp;
+    newNode->left=newNode->right=NULL;
+    return newNode;
+}
