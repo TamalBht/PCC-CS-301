ad26a55a4db1ac9c5e5f2c66751a8baf3017c8d8
Tamal Bhattacharjee <119165001+TamalBht@users.noreply.github.com>
2024-01-31 17:18:18 +0530

    change

--- levelOrderTraversal.c
+++ levelOrderTraversal.c
@@ -13,4 +13,13 @@ Node* createNode(int inp){
     newNode->left=newNode->right=NULL;
     return newNode;
 }
-
+//returns pointer to the address of node
+Node** createQueue(int* front,int* rear){
+    Node** queue= (Node**)malloc(MAX);
+    *front=*rear=0;
+    return queue;
+}
+//parameters for enqueue are the queue itself the front pointer of the node and the node itself
+void enq( Node**queue,int* front,Node* newNode){
+    
+}
