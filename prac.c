9563e0c055e59263fd38ea7935765a13718156d2
Tamal Bhattacharjee <119165001+TamalBht@users.noreply.github.com>
2024-02-03 21:46:21 +0530

    change

--- prac.c
+++ prac.c
@@ -31,4 +31,37 @@ Node* findLastRight(Node* root);
 Node* delete(Node*root,int key){
     if(root==NULL) return NULL;
     if(root->data==key) return helper(root);
+    //only one node exists
+    Node* dummy= root;
+    while(root!=NULL){
+        if(root->data>key){
+            if(root->left->data==key && root->left!=NULL){
+                 root->left=helper(root->left);
+                break;
+            }
+            else root=root->left;
+
+        }
+        else if(root->data<key){
+            if(root->right!=NULL && root->right->data==key){
+                root->right=helper(root->right);
+                break;
+            }
+            else root=root->right;
+        }
+    }
+    return dummy;
+}
+Node* helper(Node* root){
+    if(root->right==NULL) return root->left;
+    else if(root->left==NULL) return root->right;
+    //to select the right subtree
+    Node* rightChild= root->right;
+    Node* rightMost=findLastRight(root->left);
+    rightMost->right=rightChild;
+    return root->left;//returns the parent element to be deleted
+}
+Node* findLastRight(Node*root){
+    if(root->right==NULL) return root;
+    else findLastRight(root->right);
 }
