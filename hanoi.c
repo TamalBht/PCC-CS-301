149b8bae1ccd698631835419a9a276957b72ee00
Tamal Bhattacharjee <119165001+TamalBht@users.noreply.github.com>
2024-02-05 10:33:48 +0530

    change

--- hanoi.c
+++ hanoi.c
@@ -3,6 +3,14 @@
 void toh(int n,char from,char to,char aux){
     if(n==0) return;
     toh(n-1,from,aux,to);
-    printf("Move disc  %d from rod %c to rod %c",n,from,to);
+    printf("Move disc  %d from rod %c to rod %c\n",n,from,to);
     toh(n-1,aux,to,from);
+}
+int main(){
+    int num;
+    printf("ENter number of discs : ");
+    scanf("%d",&num);
+    char A='A',B='B',C='C';
+    toh(num,A,B,C);
+    return 0;
 }
