cb05cda63cc0e2fceb6640b8bc6f242857dcc9e4
Tamal Bhattacharjee <119165001+TamalBht@users.noreply.github.com>
2024-02-05 10:27:46 +0530

    change

---
+++ hanoi.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+#include<stdlib.h>
+void toh(int n,char from,char to,char aux){
+    if(n==0) return;
+    toh(n-1,from,aux,to);
+    printf("Move disc  %d from rod %c to rod %c",n,from,to);
+    toh(n-1,aux,to,from);
+}
