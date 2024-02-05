85f411a4e819e51b28ed9e8480dcd37b30167e9f
Tamal Bhattacharjee <119165001+TamalBht@users.noreply.github.com>
2024-02-05 16:32:29 +0530

    change

--- priorityQueue.c
+++ priorityQueue.c
@@ -19,3 +19,23 @@ int isFull(Queue* q){
 int isEmpty(Queue* q){
     return(q->size==0);
 }
+
+void enqueue(Queue* q){
+    if (isFull(q)) {
+        printf("Queue is full. Enqueue operation failed.\n");
+        return;
+    }
+    int inp,p;
+    printf("Enter data and pririty followed by a space: ");
+    scanf("%d%d",&inp,&p);
+    Element newEle;
+    newEle.data=inp;
+    newEle.priority=p;
+    //now in priority queue elements are enqueued or dequeued based on priority
+    //so well run a while loop from the end to the start till the priority of new element is less thaan the existing priority element
+    int i=q->size-1;
+    while(newEle.priority>q->item[i].priority && i>=0){
+        
+    }
+
+}
